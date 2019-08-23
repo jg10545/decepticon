8434d52f81e0f6d27a8d13a994e3103f791591fd
Joe <joegezo@gmail.com>
2019-08-23 19:30:23 -0400

    tweaks to style loss and inpainter pretraining

--- decepticon/_synthetic.py
+++ decepticon/_synthetic.py
@@ -22,6 +22,19 @@ def _rand_rectangle(H,W):
     return top, left, top+dh, left+dw
 
 
+
+def mask_img(img, num_empty):
+    """
+    Cut num_empty random rectangles out of an image
+    """
+    img = img.copy()
+    for n in range(num_empty):
+        top, left, bottom, right = _rand_rectangle(img.shape[0], img.shape[1])
+        img[top:bottom, left:right, :] = 0
+    return img
+    
+
+
 def _generate_img(imshape, label, num_rectangles=25, num_empty=0):
     """
     generate an image that's random noise with some random 
@@ -36,10 +49,9 @@ def _generate_img(imshape, label, num_rectangles=25, num_empty=0):
     for n in range(num_rectangles):
         top, left, bottom, right = _rand_rectangle(imshape[0], imshape[1])
         img[top:bottom, left:right, np.random.randint(0,m)] = 0
-        
-    for n in range(num_empty):
-        top, left, bottom, right = _rand_rectangle(imshape[0], imshape[1])
-        img[top:bottom, left:right, :] = 0
+    
+    if num_empty > 0:
+        img = mask_img(img, num_empty)
         
     return img
 
@@ -77,3 +89,26 @@ def build_synthetic_dataset(prob=0.5, imshape=(128,128), num_rectangles=25,
                                    output_shapes=(imshape[0], imshape[1], 3))
     return ds
 
+
+
+
+def build_inpainter_dataset(imshape=(128,128), num_rectangles=25, 
+                            num_empty=10):
+    """
+    Synthetic dataset for inpainter pretraining. 
+    
+    :imshape: image dimensions
+    :num_rectangles: how many rectangles to draw per image
+    :num_empty: number of empty rectangles to draw (for pretraining object
+                classifier)
+    :return_labels: whether dataset generates (image, label) tuples or just images
+    """
+    def _example_generator():
+        while True:
+            img = _generate_img(imshape, 0, num_rectangles, num_empty=0)
+            yield mask_img(img, num_empty), img
+    
+    ds = tf.data.Dataset.from_generator(_example_generator, (tf.float32, tf.float32),
+                                   output_shapes=((imshape[0], imshape[1], 3), 
+                                                  (imshape[0], imshape[1], 3)))
+    return ds
