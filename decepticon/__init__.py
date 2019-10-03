8d31320a415f0509e8bd4a2c8119223b0c35347c
Joe <joegezo@gmail.com>
2019-10-03 08:29:33 -0400

    updated init

--- decepticon/__init__.py
+++ decepticon/__init__.py
@@ -7,7 +7,9 @@ __email__ = 'joe@gezo.net'
 __version__ = '0.1.0'
 
 
-from decepticon._models import build_inpainter, build_classifier, build_mask_generator, build_discriminator
+from decepticon._models import build_inpainter, build_classifier
+from decepticon._models import build_mask_generator 
+from decepticon._models import build_discriminator, build_mask_discriminator
 from decepticon._synthetic import build_synthetic_dataset, build_inpainter_dataset
 from decepticon._trainers import Trainer
 import decepticon.loaders
